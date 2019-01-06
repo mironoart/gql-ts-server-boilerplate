385bcebce7fe91fc7f06b22b28c13d5fed783fff
Andry Mironov <andry.mironov94@gmail.com>
2019-01-06 23:57:10 +0200

    1.2 TypeORM Entity

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import "reflect-metadata";
-
+import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
+import { resolvers } from "./resolvers";
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "a"}`
-  }
-};
-
+const typeDefs = importSchema("schema.graphql");
+// also can do instead of const typeDefs = importSchema... -- GraphQLServer({ typeDefs: 'src/schema.graphql'....
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log("Server is running on localhost:4000"));
