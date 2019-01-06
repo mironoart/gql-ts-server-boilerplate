46a48496f9f8045f7ffb0208a24155e8f856d5fa
Andry Mironov <andry.mironov94@gmail.com>
2019-01-06 22:36:15 +0200

    1.1 Setting up TypeORM Project

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "reflect-metadata";
+
+import { GraphQLServer } from "graphql-yoga";
+
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_: any, { name }: any) => `Hello ${name || "Bye"}`
+  }
+};
+
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
